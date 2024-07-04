f647447a2201b28ec0c0c39704f754198d5b49a5
Heena Varu <heena.nvr@gmail.com>
2024-07-04 17:51:00 +0200

    review

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 #
 # Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#   ['Action', 'Comedy', 'Drama', 'Horror'].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 # require 'open-uri'
@@ -12,34 +12,42 @@
 
 # # Replace 'YOUR_API_KEY' with your actual API key
 # API_KEY = '7b18dac811bb30989dfc2990de6eaebf'
-# API_URL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&api_key=#{API_KEY}"
+# API_URL = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&api_key=#{API_KEY}'
 
 # def fetch_movies
 #   response = open(API_URL)
 #   data = JSON.parse(response.read)
-#   data["results"]
+#   data['results']
 # end
 
 # def create_movie(movie_data)
 #   Movie.create!(
-#     title: movie_data["title"],
-#     overview: movie_data["overview"],
-#     poster_url: "https://image.tmdb.org/t/p/w500#{movie_data["poster_path"]}",
-#     rating: movie_data["vote_average"]
+#     title: movie_data['title'],
+#     overview: movie_data['overview'],
+#     poster_url: 'https://image.tmdb.org/t/p/w500#{movie_data['poster_path']}',
+#     rating: movie_data['vote_average']
 #   )
 # end
 
 # # Fetch movies from the API and seed the database
 # movies = fetch_movies
 # movies.each do |movie_data|
-#   create_movie(movie_data) unless Movie.exists?(title: movie_data["title"])
+#   create_movie(movie_data) unless Movie.exists?(title: movie_data['title'])
 # end
 
-# puts "============================================="
-# puts "#{movies.count} movies seeded from the API."
+# puts '============================================='
+# puts '#{movies.count} movies seeded from the API.'
 
 # db/seeds.rb
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+Movie.create(title: 'Wonder Woman 1984', overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s', poster_url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', rating: 6.9)
+Movie.create(title: 'The Shawshank Redemption', overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison', poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg', rating: 8.7)
+Movie.create(title: 'Titanic', overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg', rating: 7.0)
+
+bookmarks_data = [
+  { list_id: 2, movie_id: 1, comment: 'good feel action movie!' },
+  { list_id: 2, movie_id: 2, comment: 'A classic movie' }
+]
+bookmarks_data.each do |data|
+  Bookmark.create(data)
+end
