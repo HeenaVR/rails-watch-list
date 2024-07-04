512c4973d848bcf10867af9f54c82f35dc6d9b04
Heena Varu <heena.nvr@gmail.com>
2024-07-04 21:56:48 +0200

    basic styling in listing

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,4 @@
 class ListsController < ApplicationController
-
   before_action :set_list, only: [:show, :destroy]
 
   def index
@@ -18,24 +17,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    # @list.destroy
-    # redirect_to lists_path, status: :see_other
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
-  def list_params
-    params.require(:list).permit(:name)
-  end
-
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
   end
 end
