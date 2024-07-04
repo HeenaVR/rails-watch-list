bf3f2f2c8106659933390e4617e68b6568812575
Heena Varu <heena.nvr@gmail.com>
2024-07-04 14:45:36 +0200

    Added seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# require 'open-uri'
+# require 'json'
+
+# # Replace 'YOUR_API_KEY' with your actual API key
+# API_KEY = '7b18dac811bb30989dfc2990de6eaebf'
+# API_URL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&api_key=#{API_KEY}"
+
+# def fetch_movies
+#   response = open(API_URL)
+#   data = JSON.parse(response.read)
+#   data["results"]
+# end
+
+# def create_movie(movie_data)
+#   Movie.create!(
+#     title: movie_data["title"],
+#     overview: movie_data["overview"],
+#     poster_url: "https://image.tmdb.org/t/p/w500#{movie_data["poster_path"]}",
+#     rating: movie_data["vote_average"]
+#   )
+# end
+
+# # Fetch movies from the API and seed the database
+# movies = fetch_movies
+# movies.each do |movie_data|
+#   create_movie(movie_data) unless Movie.exists?(title: movie_data["title"])
+# end
+
+# puts "============================================="
+# puts "#{movies.count} movies seeded from the API."
+
+# db/seeds.rb
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
