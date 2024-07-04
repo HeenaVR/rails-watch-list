02ec299f40f4864a876d1963ce9f4fa05ea7cc32
Heena Varu <heena.nvr@gmail.com>
2024-07-04 23:28:42 +0200

    basic bookmark list

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,15 +39,36 @@
 # puts '#{movies.count} movies seeded from the API.'
 
 # db/seeds.rb
-Movie.create(title: 'Wonder Woman 1984', overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s', poster_url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', rating: 6.9)
-Movie.create(title: 'The Shawshank Redemption', overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison', poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg', rating: 8.7)
-Movie.create(title: 'Titanic', overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg', rating: 7.0)
+Movie.create(title: 'Wonder Woman 1984',
+             overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s',
+             poster_url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg',
+             rating: 6.9)
+Movie.create(title: 'The Shawshank Redemption',
+             overview: 'Framed in the 1940s for double murder',
+             poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+             rating: 8.7)
+Movie.create(title: 'Titanic',
+             overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.',
+             poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg',
+             rating: 7.9)
+Movie.create(title: "Ocean's Eight",
+             overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
+             poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg',
+             rating: 7.0)
 
-bookmarks_data = [
-  { list_id: 2, movie_id: 1, comment: 'good feel action movie!' },
-  { list_id: 2, movie_id: 2, comment: 'A classic movie' }
-]
-bookmarks_data.each do |data|
-  Bookmark.create(data)
-end
+# bookmarks_data = [
+#   { list_id: 2, movie_id: 1, comment: 'good feel action movie!' },
+#   { list_id: 2, movie_id: 2, comment: 'A classic movie' }
+# ]
+
+movie = Movie.find_by(title: 'Wonder Woman 1984')
+list1 = List.find(2)
+list1.bookmarks.create(movie: movie, comment: "Great action sequences!")
+
+movie2 = Movie.find_by(title: 'The Shawshank Redemption')
+list2 = List.find(3)
+list2.bookmarks.create(movie: movie2, comment: "A must-watch classic!")
+
+# bookmarks_data.each do |data|
+#   Bookmark.create(data)
+# end
